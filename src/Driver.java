acb72fc4ec8742537d03ab69f832d9bbc50d9d08
Noah Obasi <nco170230@utdallas.edu>
2021-04-29 17:36:53 -0500

    Added a driver

---
+++ src/Driver.java
@@ -0,0 +1,9 @@
+/**
+ * Given two integers, compute their greatest common divisor
+ */
+public class Driver {
+	public static void main(String[] args) {
+	    System.out.println("Hello World");
+	}
+
+}
